9630e3bdb15248d69ae5af9b4e7165d9ef3e46ae
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-05-03 13:48:18 +0900

    ✨ feat: gpt summally

---
+++ src/theme/DocItem/Content/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import axios from "axios";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import { useDoc } from "@docusaurus/theme-common/internal";
+import Heading from "@theme/Heading";
+import MDXContent from "@theme/MDXContent";
+/**
+ Title can be declared inside md content or declared through
+ front matter and added manually. To make both cases consistent,
+ the added title is added under the same div.markdown block
+ See https://github.com/facebook/docusaurus/pull/4882#issuecomment-853021120
+
+ We render a "synthetic title" if:
+ - user doesn't ask to hide it with front matter
+ - the markdown content does not already contain a top-level h1 heading
+*/
+function useSyntheticTitle() {
+  const { metadata, frontMatter, contentTitle } = useDoc();
+  const shouldRender =
+    !frontMatter.hide_title && typeof contentTitle === "undefined";
+  if (!shouldRender) {
+    return null;
+  }
+  return metadata.title;
+}
+
+const sendRequest = async (editUrl) => {
+  try {
+    const requestBody = {
+      prompt: editUrl,
+    };
+    const response = await axios.post(
+      "https://gpt-hono.yutakobayashi.workers.dev/openai/ask",
+      requestBody
+    );
+    return response.data.message;
+  } catch (error) {
+    console.error("Error sending request:", error);
+    return "エラーが発生しました。リクエストを送信できませんでした。";
+  }
+};
+
+export default function DocItemContent({ children }) {
+  const { metadata } = useDoc();
+
+  const [message, setMessage] = useState("");
+  const [isSummarizing, setIsSummarizing] = useState(false);
+
+  const handleClick = async () => {
+    setIsSummarizing(true);
+    const responseMessage = await sendRequest(metadata.editUrl);
+    setMessage(responseMessage);
+    setIsSummarizing(false);
+  };
+
+  const syntheticTitle = useSyntheticTitle();
+
+  return (
+    <div className={clsx(ThemeClassNames.docs.docMarkdown, "markdown")}>
+      {syntheticTitle && (
+        <header>
+          <Heading as="h1">{syntheticTitle}</Heading>
+        </header>
+      )}
+      <div className="gpt-summally">
+        <button onClick={handleClick}>
+          {isSummarizing ? "要約中..." : "AIで要約"}
+        </button>
+        {message && <p className="gpt-summally-text">{message}</p>}
+        <p>
+          OpenAIのAPIを使用しています。正確性の保証はできませんので、ご注意ください。
+        </p>
+      </div>
+      <MDXContent>{children}</MDXContent>
+    </div>
+  );
+}
